60810b302d9f35ec44201ba3ccf9b3e4050d940d
isjmovo <isjmovo@gmail.com>
2022-08-14 01:05:33 +0900

    명령 입력받기

--- src/com/kjm/exam08/Board/Main.java
+++ src/com/kjm/exam08/Board/Main.java
@@ -1,7 +1,47 @@
 package com.kjm.exam08.Board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("===== 프로그램 실행 =====");
+    System.out.println("===== 게시판 v 0.1 =====");
+
+    while (true) {
+      System.out.printf("명령을 입력하세요: ");
+      String cmd = sc.nextLine();
+
+      System.out.println("입력된 명령: " + cmd);
+
+      if (cmd.equals("/usr/article/write")) {
+
+        int num = 0;
+
+        System.out.println("===== 게시물 등록 =====");
+
+        System.out.printf("제목: ");
+        String title = sc.nextLine();
+
+        System.out.printf("내용: ");
+        String content = sc.nextLine();
+
+        num++;
+        System.out.println(num + "번째 게시물이 등록되었습니다.");
+      }
+
+      else if (cmd.equals("exit")) {
+        break;
+      }
+
+      else {
+
+      }
+      sc.close();
+
+      System.out.println("===== 프로그램 종료 =====");
+    }
   }
 }
