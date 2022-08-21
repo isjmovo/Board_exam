d3d0a8b9c85ecb282b5b3a36903f6e0bfb7d264c
isjmovo <isjmovo@gmail.com>
2022-08-21 14:09:16 +0900

    특정 명령 입력 받아 최근 게시물 출력

--- src/com/kjm/exam08/Board/Main.java
+++ src/com/kjm/exam08/Board/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
     int num = 0;
+    Article lastAc = null;
 
     System.out.println("===== 프로그램 실행 =====");
     System.out.println("===== 게시판 v 0.1 =====");
@@ -14,8 +15,6 @@ public class Main {
     while (true) {
       System.out.printf("명령을 입력하세요: ");
       String cmd = sc.nextLine();
-
-      System.out.println("입력된 명령: " + cmd);
 
       if (cmd.equals("/usr/article/write")) {
 
@@ -30,6 +29,7 @@ public class Main {
         num++;
 
         Article ac = new Article(num, title, content);
+        lastAc = ac;
 
         System.out.printf("%d번째 게시물이 입력되었습니다.\n", ac.num);
         System.out.println(ac);
@@ -37,6 +37,25 @@ public class Main {
 
       else if (cmd.equals("exit")) {
         break;
+      }
+
+      else if (cmd.equals("/usr/article/detail")) {
+        if (lastAc == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article ac = lastAc;
+
+        System.out.println("===== 게시물 상세 내용 =====");
+
+        System.out.printf("번호: %d\n", lastAc.num);
+        System.out.printf("제목: %s\n", lastAc.title);
+        System.out.printf("내용: %s\n", lastAc.content);
+      }
+
+      else {
+        System.out.println("입력된 명령: " + cmd);
       }
     }
 
